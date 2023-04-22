b073f7bf730101a36c93e77b04aef0609b7d5037
Bruno <brunocosta.1991@hotmail.com>
2023-04-22 12:51:32 -0300

    add

--- curso_python/aula1.py
+++ curso_python/aula1.py
@@ -1,1 +1,13 @@
-print("Olá mundo")
+"""
+DocString
+E escrever o que eu
+quiser
+asdfasdfd
+"""
+
+''' Usar para escrever suas notas '''
+
+# Permite escrever um comentário
+print(123)  # Na frente
+# Abaixo
+print(456)
